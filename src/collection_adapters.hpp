6480a8281137bd159dc0e26b58f6f62d89f7f2bb
Andreas Reich <r_andreas2@web.de>
2023-11-27 13:14:36 +0100

    add assertion, comment improvements

--- src/collection_adapters.hpp
+++ src/collection_adapters.hpp
@@ -5,13 +5,17 @@
 #include <eigen3/Eigen/Core>
 #include <opencv2/core.hpp>
 
+#include <cassert>
+
 // Adapters so we can log eigen vectors as rerun positions:
 template <>
 struct rerun::CollectionAdapter<rerun::Position3D, std::vector<Eigen::Vector3f>> {
+    /// Borrow for non-temporary.
     Collection<rerun::Position3D> operator()(const std::vector<Eigen::Vector3f>& container) {
         return Collection<rerun::Position3D>::borrow(container.data(), container.size());
     }
 
+    // Do a full copy for temporaries (otherwise the data will be deleted when the temporary is destroyed).
     Collection<rerun::Position3D> operator()(std::vector<Eigen::Vector3f>&& container) {
         std::vector<rerun::Position3D> positions(container.size());
         memcpy(positions.data(), container.data(), container.size() * sizeof(Eigen::Vector3f));
@@ -22,12 +26,14 @@ struct rerun::CollectionAdapter<rerun::Position3D, std::vector<Eigen::Vector3f>>
 // Adapters so we can log an eigen matrix as rerun positions:
 template <>
 struct rerun::CollectionAdapter<rerun::Position3D, Eigen::Matrix3Xf> {
+    // Sanity check that this is binary compatible.
+    static_assert(
+        sizeof(rerun::Position3D) ==
+        sizeof(Eigen::Matrix3Xf::Scalar) * Eigen::Matrix3Xf::RowsAtCompileTime
+    );
+
+    /// Borrow for non-temporary.
     Collection<rerun::Position3D> operator()(const Eigen::Matrix3Xf& matrix) {
-        // Sanity check that this is binary compatible.
-        static_assert(
-            sizeof(rerun::Position3D) ==
-            sizeof(Eigen::Matrix3Xf::Scalar) * Eigen::Matrix3Xf::RowsAtCompileTime
-        );
         static_assert(alignof(rerun::Position3D) <= alignof(Eigen::Matrix3Xf::Scalar));
         return Collection<rerun::Position3D>::borrow(
             // Cast to void because otherwise Rerun will try to do above sanity checks with the wrong type (scalar).
@@ -36,6 +42,7 @@ struct rerun::CollectionAdapter<rerun::Position3D, Eigen::Matrix3Xf> {
         );
     }
 
+    // Do a full copy for temporaries (otherwise the data will be deleted when the temporary is destroyed).
     Collection<rerun::Position3D> operator()(Eigen::Matrix3Xf&& matrix) {
         std::vector<rerun::Position3D> positions(matrix.cols());
         memcpy(positions.data(), matrix.data(), matrix.size() * sizeof(rerun::Position3D));
@@ -46,11 +53,21 @@ struct rerun::CollectionAdapter<rerun::Position3D, Eigen::Matrix3Xf> {
 // Adapters so we can borrow an OpenCV image easily into Rerun images without copying:
 template <>
 struct rerun::CollectionAdapter<uint8_t, cv::Mat> {
+    /// Borrow for non-temporary.
     Collection<uint8_t> operator()(const cv::Mat& img) {
+        assert(
+            "OpenCV matrix was expected have bit depth CV_U8" && CV_MAT_DEPTH(img.type()) == CV_8U
+        );
+
         return Collection<uint8_t>::borrow(img.data, img.total() * img.channels());
     }
 
+    // Do a full copy for temporaries (otherwise the data will be deleted when the temporary is destroyed).
     Collection<uint8_t> operator()(cv::Mat&& img) {
+        assert(
+            "OpenCV matrix was expected have bit depth CV_U8" && CV_MAT_DEPTH(img.type()) == CV_8U
+        );
+
         std::vector<uint8_t> img_vec(img.total() * img.channels());
         img_vec.assign(img.data, img.data + img.total() * img.channels());
         return Collection<uint8_t>::take_ownership(std::move(img_vec));
@@ -61,8 +78,8 @@ struct rerun::CollectionAdapter<uint8_t, cv::Mat> {
 // TODO(https://github.com/rerun-io/rerun/pull/4331): remove `datatypes::`
 template <>
 struct rerun::CollectionAdapter<rerun::datatypes::TensorDimension, cv::Mat> {
+    /// Only overload the const& operator since there is no way of borrowing the dimensions anyways.
     Collection<rerun::datatypes::TensorDimension> operator()(const cv::Mat& img) {
-        // Only specify the const& operator since there is no way of borrowing the dimensions anyways.
         return {
             static_cast<size_t>(img.rows),
             static_cast<size_t>(img.cols),
